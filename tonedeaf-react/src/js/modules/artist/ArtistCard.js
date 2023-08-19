dc0601a66f9fe7d508c64717baf850f7f411db62
mlmar <marcloussm@gmail.com>
2023-08-19 03:37:08 -0700

    Removed artist follower count

--- tonedeaf-react/src/js/modules/artist/ArtistCard.js
+++ tonedeaf-react/src/js/modules/artist/ArtistCard.js
@@ -14,7 +14,7 @@ const ArtistCard = (artist) => {
       <ImageWrapper src={artist?.images?.[0]?.url} title={name} nohover={!artist?.imageClick}/>
       <div className="description flex-col">
         <label className="large bold"> {name} </label>
-        <label className="medium"> {artist?.followers?.total?.toLocaleString()} followers </label>
+        {/* <label className="medium"> {artist?.followers?.total?.toLocaleString()} followers </label> */}
         <label className="medium inactive"> {artist?.genres?.join(", ")} </label>
         <div className="flex-col flex-fill flex-reverse">
           <ImageWrapper className="icon" src={ICON} title={`Open ${artist?.name} in Spotify`} url={artist?.uri} id="spotify" nohover/>
