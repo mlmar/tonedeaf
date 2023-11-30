743328646202677ca1b640b0a1821b7b0c5b65e3
mlmar <marcloussm@gmail.com>
2023-11-29 19:51:51 -0800

    Removed forcefully opening image in new tab since its considered a popup

--- tonedeaf-react/src/js/util/DownloadUtil.js
+++ tonedeaf-react/src/js/util/DownloadUtil.js
@@ -22,16 +22,11 @@ export const useDownload = () => {
       letterRendering: true,
       backgroundColor: '#131313'
     });
-    if(navigator.canShare) {
+    if(isMobile && navigator.canShare) {
       const blob = await new Promise(resolve => canvas.toBlob(resolve));
       const files = [new File([blob], 'tonedeaf.png', { type: blob.type })]
 
-      const a = document.createElement('a');
-      a.setAttribute('href', URL.createObjectURL(blob));
-      a.setAttribute('target', '_blank');
-      a.setAttribute('rel', 'noopener');
-      a.click();
-      a.remove();
+      window.location.replace(URL.createObjectURL(blob));
 
       const shareData = {
         files
